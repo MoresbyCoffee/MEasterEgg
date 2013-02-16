597dbd1fa8bf907df56ab5880e71d332d991a591
Balazs Balazs <balazs.balazs80@gmail.com>
2013-02-16 14:46:06 +0000

    Initial commit

---
+++ gen/org/moresbycoffee/measteregg/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.moresbycoffee.measteregg.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
