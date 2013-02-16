8f3d0b414273baee4b92504d8314197b7823938e
Balazs Balazs <balazs.balazs80@gmail.com>
2013-02-16 15:11:26 +0000

    remove files

--- gen/org/moresbycoffee/measteregg/android/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.moresbycoffee.measteregg.android;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
